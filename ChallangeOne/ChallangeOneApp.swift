7b266cfa7e8418402aa97d8aaba4d6a91afe2d67
Mohamed ELkilany <mohamedsayedelkilany@gmail.com>
2022-11-02 16:00:58 +0200

    Initial Commit

---
+++ ChallangeOne/ChallangeOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallangeOneApp.swift
+//  ChallangeOne
+//
+//  Created by Elkilany on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChallangeOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallangeOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChallangeOne
+//
+//  Created by Elkilany on 02/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
